8bbf03f05f3b384d3b4a11c5949776318c4be9f5
manuabrego <ms.abrego@alumno.etec.um.edu.ar>
2025-04-22 12:34:27 -0300

    Paso 2: Variables y tipos de datos

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,13 +1,36 @@
 package org.example 
-/*
-fun main() {
-    println("¡Hola, Kotlin!")
-}
-*/ 
 
+fun main() {
+    println("pepe hongo leproso")
+}
+
+/*
 val nombre= "Manuel Abrego"
 var dia="22 de abril, 2025 años después de Cristo"
 fun main() {
     println(nombre)
     println(dia)
 }
+
+
+fun mostrarTiposDeDatos() {
+    val nombre: String = "Juan"
+    val edad: Int = 25
+    val altura: Double = 1.75
+    val esEstudiante: Boolean = true
+    
+    println("Nombre: $nombre")
+    println("Edad: $edad")
+    println("Altura: $altura")
+    println("Es estudiante: $esEstudiante")
+}
+*/ 
+
+fun main() {
+    val base: Double = 2.82
+    val alt: Double = 1.2
+    val area: Double = base*alt
+
+    println ("El area es: $area")
+}
+
