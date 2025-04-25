57c393d66e0278dab6ec547c3bf492263673e109
manuabrego <ms.abrego@alumno.etec.um.edu.ar>
2025-04-25 10:05:39 -0300

    Paso 4: Loops

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -41,7 +41,7 @@ fun esParOImpar(numero: Int): String {
         else -> "Impar"
     }
 }
-*/
+
 fun esParOImpar(numero: Int): String {
     return if (numero != 0) {
         if (numero % 2 == 0) {
@@ -54,3 +54,51 @@ fun esParOImpar(numero: Int): String {
     }
 }
 
+fun mostrarNumerosConLoops() {
+    // Usando for
+    println("Usando for:")
+    for (i in 1..10) {
+        println(i)
+    }
+    
+    // Usando while
+    println("\nUsando while:")
+    var j = 1
+    while (j <= 10) {
+        println(j)
+        j++
+    }
+    
+    // Usando do-while
+    println("\nUsando do-while:")
+    var k = 1
+    do {
+        println(k)
+        k++
+    } while (k <= 10)
+}
+*/
+
+fun mostrarNumerosConLoops() {
+    
+    println("Usando for ")
+    for (i in 2..20) {
+        println(i)
+    }
+    
+    
+    println("Usando while ")
+    var j = 2
+    while (j <= 20) {
+        println(j)
+        j++ 
+    }
+    
+    
+    println("Usando do-while ")
+    var k = 2
+    do {
+        println(k)
+        k++
+    } while (k <= 20)
+}
